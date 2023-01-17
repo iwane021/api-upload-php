fe12a987eb3244b0ca444aaf19974f85277c2f12
iwaneprs <iwane.prasetiyo@gmail.com>
2023-01-18 05:37:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 
+$link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
 echo '<h1>Welcome to API Project</h1>';
+echo '<h2>URL : ' . $link . '</h2>';
 die;
